718f4f2fb628cc91e0750edf9fca369c5dfd98f0
Adriano Salles <adrianogsallesf@gmail.com>
2020-06-06 13:28:21 -0300

    first time

---
+++ services/constants/tipoDeInput.js
@@ -0,0 +1,12 @@
+export const tiposDeInputs = {
+    EMAIL: "email",
+    SENHA: "senha",
+    CPF: "cpf",
+    RG: "rg",
+    CEP: "cep",
+    LOGRADOURO: "logradouro",
+    CIDADE: "cidade",
+    ESTADO: "estado",
+    DATA_NASCIMENTO: "dataNascimento",
+};
+  
